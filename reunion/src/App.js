016a399d31ac8479ffd573eac910ec4ce17fc2bc
Dwayne Obenshain <bearmech1@gmail.com>
2017-12-17 19:39:45 -0600

    fixed logo

--- reunion/src/App.js
+++ reunion/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import steer from './Steer2.jpg';
 import './App.css';
+// import loginfield from './login.html'
 
 
 
@@ -9,14 +10,18 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          
           <h1 className="App-title">Farwell Class of 78</h1>
+          <img src={steer} className="App-logo" alt="logo" />
         </header>
         <p className="App-intro">
         <h1>Welcome</h1>
         </p>
+        <div>
         <button className = "register">Register</button>
+        </div>
         <button className = "login">Login</button>
+
       </div>
 
     );
