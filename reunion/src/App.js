b7b655b03a776a158396d0b95293fcc8001f92fe
Dwayne Obenshain <bearmech1@gmail.com>
2017-12-21 18:24:54 -0600

    converted landing page its own file

--- reunion/src/App.js
+++ reunion/src/App.js
@@ -1,37 +1,6 @@
-import React, { Component } from 'react';
-import steer from './Steer2.jpg';
-import './App.css';
-import Login from "./components/login.js"
-import Registration from "./components/Registration.js"
+import React from "react";
+import Landing from "./Landing";
 
-
-
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          
-          <h1 className="App-title">Farwell Class of 78</h1>
-          
-        </header>
-        <div className="App-intro">
-        Welcome
-        </div>
-        <img src={steer} className="App-logo" alt="logo" />
-        <div>
-      
-        </div>
-        <Registration />
-        <Login />
-        
-        
-        
-      </div>
-
-    );
-  }
-}
+const App = () => <Landing />;
 
 export default App;
